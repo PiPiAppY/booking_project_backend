19d6af1a40dcc69aeb9260a97e3a784ae888c83c
Артём Титов <163295714+PiPiAppY@users.noreply.github.com>
2026-04-17 23:26:48 +0300

    Update index.js

--- index.js
+++ index.js
@@ -8,9 +8,7 @@ const { Pool } = require('pg');
 
 const app = express();
 const cors = require('cors');
-app.use(cors({
-    origin: 'https://69e25cd21790e60009170b3f--charming-tanuki-2e5118.netlify.app'  // ваш URL от Netlify
-})); // разрешает все запросы с любых источников (для разработки)
+app.use(cors()); // разрешает все запросы с любых источников (для разработки)
 
 // Используем переменные окружения для подключения к БД
 const pool = new Pool({
